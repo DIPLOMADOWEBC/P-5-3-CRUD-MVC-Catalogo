ea95c5b4570d4caba7f7bd9998da67178a2b9806
Juan Esteban Gonzalez <e-gonzalez@outlook.com>
2020-08-29 18:28:20 -0400

    Agregar archivos de proyecto.

---
+++ P5-1Dipl_MVC_UASD_Estudiante_ImageUpload/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace P5_1Dipl_MVC_UASD_Estudiante_ImageUpload
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ P5-1Dipl_MVC_UASD_Estudiante_ImageUpload/Controllers/ClientesController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using P5_1Dipl_MVC_UASD_Estudiante_ImageUpload.Models;
+
+namespace P5_1Dipl_MVC_UASD_Estudiante_ImageUpload.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private CatalogoDBEntities db = new CatalogoDBEntities();
+
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View(db.Clientes.ToList());
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clientes clientes = db.Clientes.Find(id);
+            if (clientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientes);
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create1111([Bind(Include = "ClienteId,Nombres,Apellidos,Direccion,Email,Telefono,Movil,ImageUrl")] Clientes clientes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clientes.Add(clientes);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(clientes);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file != null)
+                {
+                    string ImageName = System.IO.Path.GetFileName(file.FileName);
+                    string physicalPath = Server.MapPath("~/Images/" + ImageName);
+                    file.SaveAs(physicalPath);
+                    Clientes cliente = new Clientes();
+                    cliente.Nombres = Request.Form["Nombres"];
+                    cliente.Apellidos = Request.Form["Apellidos"];
+                    cliente.Direccion = Request.Form["Direccion"];
+                    cliente.Email = Request.Form["Email"];
+                    cliente.Telefono = Request.Form["Telefono"];
+                    cliente.Movil = Request.Form["Movil"];
+                    cliente.ImageUrl = ImageName;
+                    if (TryValidateModel(cliente))
+                    {
+                        db.Clientes.Add(cliente);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return View("Create", cliente);
+                    }
+                }
+                return RedirectToAction("Create");
+            }
+            catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+            {
+                Exception raise = dbEx;
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        string message = string.Format("{0}:{1}",
+                            validationErrors.Entry.Entity.ToString(),
+                            validationError.ErrorMessage);
+                        // raise a new exception nesting
+                        // the current instance as InnerException
+                        raise = new InvalidOperationException(message, raise);
+                    }
+                }
+                throw raise;
+            }
+                  
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clientes clientes = db.Clientes.Find(id);
+            if (clientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientes);
+        }
+
+        // POST: Clientes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ClienteId,Nombres,Apellidos,Direccion,Email,Telefono,Movil,ImageUrl")] Clientes clientes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(clientes).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(clientes);
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Clientes clientes = db.Clientes.Find(id);
+            if (clientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientes);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Clientes clientes = db.Clientes.Find(id);
+            db.Clientes.Remove(clientes);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
